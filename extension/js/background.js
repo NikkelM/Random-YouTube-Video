753a4d1a150899b2570b5a219126ac148274dc70
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2022-12-20 14:59:03 +0100

    Fixed local storage interactions

--- extension/js/background.js
+++ extension/js/background.js
@@ -8,6 +8,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.command === "get_API_key") {
 		sendResponse(APIKey);
 	} else if (request.command === "set_API_key") {
+		console.log("Setting API key to: " + request.data.val);
 		APIKey = request.data.val;
 		setLocalStorage("API_KEY", request.data.val).then(sendResponse);
 	}
@@ -20,7 +21,7 @@ initExtension();
 // ---------- functions ----------
 
 async function initExtension() {
-	// false if the user has not set an API key
+	chrome.storage.local.get(console.log);
 	console.log("Initializing extension, getting API key...");
 	APIKey = await getFromLocalStorage("API_KEY");
 	console.log("API key: " + APIKey);
@@ -28,14 +29,14 @@ async function initExtension() {
 
 async function getFromLocalStorage(key) {
 	return await chrome.storage.local.get([key]).then((result) => {
-		if (result.key) {
-			return result.key;
+		if (result[key]) {
+			return result[key];
 		}
 		return null;
 	});
 }
 
 async function setLocalStorage(key, value) {
-	await chrome.storage.local.set({ key: value });
+	await chrome.storage.local.set({ [key]: value });
 	return value;
 }
