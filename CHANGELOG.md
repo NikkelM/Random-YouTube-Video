cf6138f25364f7b49d7b88640728874cb6a7a7f4
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2022-12-20 14:44:45 +0100

    Appended changelog, removed stale code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,9 @@
 
 ## v0.0.2
 
-- Added randomize-button to YouTube video page - find it next to the subscribe button!
+- Added randomize-button to YouTube video pages - find it next to the subscribe button!
 - The button is now smarter and will be able to tell you if something went wrong (e.g. if you forgot to add a YouTube API key - you can do so in the extension popup!)
+- You will no longer see the button change its text faster than you can read it
 - Sped up & fixed some things
 
 ## v0.0.1
--- extension/js/background.js
+++ extension/js/background.js
@@ -5,9 +5,6 @@ let APIKey = null;
 
 // Message handler
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	// if (request.command === 'get_local') {
-	// 	getFromLocalStorage(request.data.key).then(sendResponse);
-	// } else 
 	if (request.command === 'get_API_key') {
 		sendResponse(APIKey);
 	} else if (request.command === 'set_API_key') {
@@ -28,15 +25,6 @@ async function initExtension() {
 	APIKey = await getFromLocalStorage("API_KEY");
 	console.log("API key: " + APIKey);
 }
-
-// async function getAPIKey() {
-// 	return await chrome.storage.local.get(["API_KEY"]).then((result) => {
-// 		if (result.API_KEY) {
-// 			return result.API_KEY;
-// 		}
-// 		return null;
-// 	});
-// }
 
 async function getFromLocalStorage(key) {
 	return await chrome.storage.local.get([key]).then((result) => {
