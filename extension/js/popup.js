63328efd8234ae5d211995201e8db713b7ab08f6
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2022-12-28 17:55:39 +0100

    Added hover tooltips for options

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -3,10 +3,15 @@ import configSync from "./config.js";
 // ---------- Get DOM elements ----------
 
 const useCustomApiKeyOptionToggle = document.getElementById("useCustomApiKeyOptionToggle");
+const dbOptOutOptionToggle = document.getElementById("dbOptOutOptionToggle");
 
 // ---------- Set default values from config ----------
 
 useCustomApiKeyOptionToggle.checked = configSync.useCustomApiKeyOption;
+// TODO: Make sure the user is allowed to use this option
+// TODO: Custom API key must be checked, API key must be provided and != default key
+// TODO: Else classList.add("disabled")
+dbOptOutOptionToggle.checked = configSync.dbOptOutOption;
 
 // ---------- Event listeners ----------
 
@@ -15,6 +20,14 @@ useCustomApiKeyOptionToggle.addEventListener("change", function () {
 		setSyncStorageValue("useCustomApiKeyOption", true);
 	} else {
 		setSyncStorageValue("useCustomApiKeyOption", false);
+	}
+});
+
+dbOptOutOptionToggle.addEventListener("change", function () {
+	if (this.checked) {
+		setSyncStorageValue("dbOptOutOption", true);
+	} else {
+		setSyncStorageValue("dbOptOutOption", false);
 	}
 });
 
