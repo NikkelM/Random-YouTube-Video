9e524e3f3ecb31afbd6afc351f96313da6538492
NikkelM <nikkel.mollenhauer@gmx.de>
2023-01-17 18:57:54 +0100

    Fixed bug for videoId update

--- extension/js/buildShuffleButton.js
+++ extension/js/buildShuffleButton.js
@@ -77,7 +77,7 @@ function buildShuffleButton(pageType) {
 		// Unhide the button if it was hidden
 		document.getElementById(buttonDivID).style.display = "flex";
 		if(mustUpdateVideoId) {
-			document.getElementById('youtube-random-video-shuffle-button-channel').children[0].children[0].children[0].children.namedItem('videoLink').innerHTML = videoId;
+			document.getElementById(buttonDivID).children[0].children[0].children[0].children.namedItem('videoLink').innerHTML = videoId;
 		}
 
 		return;
