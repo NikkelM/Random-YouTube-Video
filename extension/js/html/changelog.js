c52c3644a4595c1ed7ff861589dc9e7e4504cbb2
NikkelM <nikkel.mollenhauer@gmx.de>
2023-05-17 15:16:34 +0200

    Added a dropdown menu to the changelog page to change which version's changes to view

--- extension/js/html/changelog.js
+++ extension/js/html/changelog.js
@@ -7,16 +7,18 @@ function getDomElements() {
 	return {
 		// The div containing all other elements
 		randomYoutubeVideo: document.getElementById("randomYoutubeVideo"),
-
-		// The heading containing the new version number
+		// The document heading with the current version
 		updateHeading: document.getElementById("updateHeading"),
-
+		// The div containing all elements below the heading
+		belowHeadingDiv: document.getElementById("belowHeadingDiv"),
+		// The heading containing the "What's new in <version>:" text
+		whatsNewHeader: document.getElementById("whatsNewHeader"),
 		// The div containing the changelog, an unordered list element
 		changelogDiv: document.getElementById("changelogDiv"),
-
+		// The dropdown menu for selecting a version
+		chooseChangelogVersionDropdown: document.getElementById("chooseChangelogVersionDropdown"),
 		// The p element containing the shuffle tip
 		shufflingTipP: document.getElementById("shufflingTipP"),
-
 		// The button that displays the next shuffle tip
 		nextTipButton: document.getElementById("nextTipButton"),
 	}
@@ -24,13 +26,14 @@ function getDomElements() {
 
 const currentVersion = chrome.runtime.getManifest().version;
 domElements.updateHeading.innerText = `Random YouTube Video - v${currentVersion}`;
+domElements.whatsNewHeader.innerText = `What's new in v${currentVersion}:`;
 
 let changelogText = null;
 await updateChangelog();
 
-async function fetchChangelog(forVersion = currentVersion) {
+async function fetchChangelog(forVersion = `v${currentVersion}`) {
 	// Get the current changelog from GitHub
-	let changelog = await fetch(`https://raw.githubusercontent.com/NikkelM/Random-YouTube-Video/v${forVersion}/CHANGELOG.md`)
+	let changelog = await fetch(`https://raw.githubusercontent.com/NikkelM/Random-YouTube-Video/${forVersion}/CHANGELOG.md`)
 		.then(response => response.text());
 
 	if (changelog === "404: Not Found") {
@@ -40,35 +43,55 @@ async function fetchChangelog(forVersion = currentVersion) {
 	return changelog;
 }
 
-async function updateChangelog(forVersion = currentVersion) {
+async function updateChangelog(forVersion = `v${currentVersion}`) {
 	if (changelogText === null) {
 		changelogText = await fetchChangelog(forVersion);
+		domElements.belowHeadingDiv.classList.remove("hidden");
 	}
 
-	// Get the text between "## v${version}" and the next "##"
-	const versionIndex = changelogText.indexOf(`## v${forVersion}`);
+	// Get the text between "## ${version}" and the next "##", or if there is none, the end of the changelog
+	const versionIndex = changelogText.indexOf(`## ${forVersion}`);
+	const nextVersionIndex = changelogText.indexOf("##", versionIndex + `## ${forVersion}`.length);
+	// If there is no next version, use the end of the changelog
+	const endIndex = nextVersionIndex !== -1 ? nextVersionIndex : changelogText.length;
 
-	changelogText = versionIndex !== -1
+	let thisVersionChangelog = versionIndex !== -1
 		? changelogText.substring(
-			versionIndex + `## v${forVersion}`.length,
-			changelogText.indexOf("##", versionIndex + `## v${forVersion}`.length))
+			versionIndex + `## v${forVersion}`.length, endIndex)
 		: "";
-	console.log(changelogText)
 
-	if (changelogText === "") {
-		changelogText = `\n- No changes found for this version (v${forVersion}).`;
+	if (thisVersionChangelog === "") {
+		thisVersionChangelog = `\n- No changes found for this version (${forVersion}).`;
 	}
 
 	// Add the changelog to the changelogDiv in the form of an unordered list, with each line being a list item, minus the leading "- "
 	const changelogList = document.createElement("ul");
 	changelogList.classList.add("thirdWidth", "textLeft");
-	changelogList.innerHTML = changelogText.replace(/^- /gm, "<li>");
+	changelogList.innerHTML = thisVersionChangelog.replace(/^- /gm, "<li>");
 
 	// Replace the current child of the changelogDiv with the new list
 	domElements.changelogDiv.children[0].replaceWith(changelogList);
 }
 
-// Logic for displaying hints
+// ----- Dropdown menu -----
+const regex = /v\d+(\.\d+)+/g;
+const versions = changelogText.match(regex);
+// Add all versions to the dropdown menu
+versions.forEach(version => {
+	const option = document.createElement("option");
+	option.value = version;
+	option.innerText = version;
+	domElements.chooseChangelogVersionDropdown.appendChild(option);
+});
+
+// Change the displayed changelog to the chosen version
+domElements.chooseChangelogVersionDropdown.addEventListener("change", async function () {
+	domElements.whatsNewHeader.innerText = `What's new in ${this.value}:`;
+	await updateChangelog(this.value);
+});
+
+
+// ----- Hints -----
 let currentHint = await displayShufflingHint(domElements.shufflingTipP);
 // Add click listener to the "New tip" button
 domElements.nextTipButton.addEventListener("click", async function () {
