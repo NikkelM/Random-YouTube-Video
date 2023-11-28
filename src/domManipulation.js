488a665c0b00deaec41f730781d45284cd4625b5
NikkelM <57323886+NikkelM@users.noreply.github.com>
2023-11-28 19:17:13 +0000

    Switched variable meaning, better tooltip for small button

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -9,7 +9,7 @@ export function buildShuffleButton(pageType, channelId, clickHandler) {
 	let buttonDivExtraStyle = "";
 	let buttonDivOwner = null;
 	let buttonDivAppend = true;
-	let isSmallButton = false;
+	let isLargeButton = true;
 
 	// Depending on the type of page we're on, we might need to change certain parts of the button
 	switch (pageType) {
@@ -23,7 +23,7 @@ export function buildShuffleButton(pageType, channelId, clickHandler) {
 			buttonDivOwner = [document.getElementById("above-the-fold").children.namedItem("top-row").children.namedItem("owner")];
 			break;
 		case "short":
-			isSmallButton = true;
+			isLargeButton = false;
 			buttonDivID = "youtube-random-video-small-shuffle-button-short";
 			buttonDivAppend = false;
 			buttonDivOwner = document.querySelectorAll("ytd-reel-video-renderer ytd-reel-player-overlay-renderer #actions");
@@ -47,9 +47,14 @@ export function buildShuffleButton(pageType, channelId, clickHandler) {
 		return;
 	}
 
+	if (!channelId) {
+		console.log("Cannot build button: No channelID found.");
+		return;
+	}
+
 	// If all required buttons already exist, don't build them again, but only update values
 	let allButtonsOnPage = document.querySelectorAll(`#${buttonDivID}`);
-	if (allButtonsOnPage.length >= buttonDivOwner.length && channelId) {
+	if (allButtonsOnPage.length >= buttonDivOwner.length) {
 		let button = null;
 		if (pageType === "short") {
 			// If we are on a shorts page, get the button of the active renderer
@@ -66,18 +71,16 @@ export function buildShuffleButton(pageType, channelId, clickHandler) {
 
 		// Set the variables to the correct button reference (channel vs. video vs. shorts page)
 		shuffleButton = button;
-		if (isSmallButton) {
-			shuffleButtonTextElement = shuffleButton.children[0].children[0].children[0].children[0].children[0];
-		} else {
+		if (isLargeButton) {
 			shuffleButtonTextElement = shuffleButton.children[0].children[0].children[0].children[1].children[0];
+		} else {
+			shuffleButtonTextElement = shuffleButton.children[0].children[0].children[0].children[0].children[0];
 		}
 
 		return;
-	}
-
-	if (!channelId) {
-		console.log("Cannot build button: No channelID found.");
-		return;
+		// Else, we need to build new buttons for those owners that don't have one yet
+	} else {
+		buttonDivOwner = Array.from(buttonDivOwner).filter(owner => !owner.children.namedItem(buttonDivID));
 	}
 
 	// Create the button div & renderer
@@ -120,7 +123,7 @@ export function buildShuffleButton(pageType, channelId, clickHandler) {
 		let shuffleButton = buttonDivOwner[buttonDivOwner.length - 1].children.namedItem(buttonDivID);
 		if (shuffleButton.children.length > 0) {
 			me.disconnect(); // Stop observing
-			finalizeButton(pageType, channelId, clickHandler, isSmallButton);
+			finalizeButton(pageType, channelId, clickHandler, isLargeButton, buttonDivOwner, buttonDivID);
 			return;
 		}
 	});
@@ -142,31 +145,30 @@ export function tryRenameUntitledList() {
 }
 
 // ----- Private -----
-function finalizeButton(pageType, channelId, clickHandler, isSmallButton) {
-	let buttonText = "&nbsp;Shuffle";
-	let buttonDivID = null;
-	let buttonDivOwner = null;
-
-	switch (pageType) {
-		case "channel":
-			buttonDivID = "youtube-random-video-large-shuffle-button-channel";
-			buttonDivOwner = [document.getElementById("inner-header-container").children.namedItem("buttons")];
-			break;
-		case "video":
-			buttonDivID = "youtube-random-video-large-shuffle-button-video";
-			buttonDivOwner = [document.getElementById("above-the-fold").children.namedItem("top-row").children.namedItem("owner")];
-			break;
-		case "short":
-			buttonDivID = "youtube-random-video-small-shuffle-button-short";
-			buttonDivOwner = document.querySelectorAll("ytd-reel-video-renderer ytd-reel-player-overlay-renderer #actions");
-			break;
-		default:
-			console.warn(`Cannot build button: unknown page type: ${pageType}`);
-			return;
-	}
-
+function finalizeButton(pageType, channelId, clickHandler, isLargeButton, buttonDivOwner, buttonDivID) {
 	let button;
-	if (isSmallButton) {
+	if (isLargeButton) {
+		button = `
+		<button
+			class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m"
+			aria-label="Shuffle from this channel">
+				<span class="material-symbols-outlined" style="width: 24.01px; overflow: hidden;">
+					shuffle
+				</span>
+				<div class="cbox yt-spec-button-shape-next--button-text-content">
+					<span id="random-youtube-video-large-shuffle-button-text" class="yt-core-attributed-string yt-core-attributed-string--white-space-no-wrap" role="text">
+						&nbsp;Shuffle
+					</span>
+				</div>
+				<yt-touch-feedback-shape style="border-radius: inherit;">
+					<div class="yt-spec-touch-feedback-shape yt-spec-touch-feedback-shape--touch-response" aria-hidden="true">
+						<div class="yt-spec-touch-feedback-shape__stroke" style></div>
+						<div class="yt-spec-touch-feedback-shape__fill" style></div>
+					</div>
+				</yt-touch-feedback-shape>
+				<span id="channelId" style="display: none">${channelId ?? ""}</span>
+		</button>`;
+	} else {
 		button = `
 		<button
 			class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-l yt-spec-button-shape-next--icon-button"
@@ -184,27 +186,6 @@ function finalizeButton(pageType, channelId, clickHandler, isSmallButton) {
 				</yt-touch-feedback-shape>
 				<span id="channelId" style="display: none">${channelId ?? ""}</span>
 		</button>`;
-	} else {
-		button = `
-	<button
-		class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m"
-		aria-label="Shuffle from this channel">
-			<span class="material-symbols-outlined" style="width: 24.01px; overflow: hidden;">
-				shuffle
-			</span>
-			<div class="cbox yt-spec-button-shape-next--button-text-content">
-				<span id="random-youtube-video-large-shuffle-button-text" class="yt-core-attributed-string yt-core-attributed-string--white-space-no-wrap" role="text">
-					${buttonText}
-				</span>
-			</div>
-			<yt-touch-feedback-shape style="border-radius: inherit;">
-				<div class="yt-spec-touch-feedback-shape yt-spec-touch-feedback-shape--touch-response" aria-hidden="true">
-					<div class="yt-spec-touch-feedback-shape__stroke" style></div>
-					<div class="yt-spec-touch-feedback-shape__fill" style></div>
-				</div>
-			</yt-touch-feedback-shape>
-			<span id="channelId" style="display: none">${channelId ?? ""}</span>
-	</button>`;
 	}
 	button = new DOMParser().parseFromString(button, "text/html").body.firstChild;
 
@@ -212,7 +193,11 @@ function finalizeButton(pageType, channelId, clickHandler, isSmallButton) {
 	<tp-yt-paper-tooltip fit-to-visible-bounds offset="8" role="tooltip" tabindex="-1">
 	</tp-yt-paper-tooltip>`;
 	buttonTooltip = new DOMParser().parseFromString(buttonTooltip, "text/html").body.firstChild;
-	buttonTooltip.innerText = "Shuffle from this channel";
+	if (isLargeButton) {
+		buttonTooltip.innerText = "Shuffle from this channel";
+	} else {
+		buttonTooltip.innerText = "Shuffle channel";
+	}
 
 	// Remove the original button tooltip, it does not have all required attributes
 	buttonDivOwner.forEach(owner => {
@@ -250,9 +235,9 @@ function finalizeButton(pageType, channelId, clickHandler, isSmallButton) {
 	}
 
 	shuffleButton = activeButton;
-	if (isSmallButton) {
-		shuffleButtonTextElement = shuffleButton.children[0].children[0].children[0].children[0].children[0];
-	} else {
+	if (isLargeButton) {
 		shuffleButtonTextElement = shuffleButton.children[0].children[0].children[0].children[1].children[0];
+	} else {
+		shuffleButtonTextElement = shuffleButton.children[0].children[0].children[0].children[0].children[0];
 	}
 }
