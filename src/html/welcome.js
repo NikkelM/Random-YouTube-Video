698646453a4c1e892fa42c6f8eba40ba2dda4389
NikkelM <57323886+NikkelM@users.noreply.github.com>
2023-11-30 20:02:50 +0000

    Added welcome page content to get started

--- src/html/welcome.js
+++ src/html/welcome.js
@@ -18,7 +18,10 @@ function getDomElements() {
 		randomYoutubeVideo: document.getElementById("randomYoutubeVideo"),
 		// The document heading with the current version
 		updateHeading: document.getElementById("updateHeading"),
-		// Displays the version text
+		// Button to reload all YouTube pages
+		reloadAllYouTubePagesButton: document.getElementById("reloadAllYouTubePagesButton"),
+		// Text displayed before/after reloading all YouTube pages
+		reloadText: document.getElementById("reloadText"),
 		// SHUFFLING HINTS
 		versionText: document.getElementById("versionText"),
 		// The p element containing the shuffle hint
@@ -30,6 +33,27 @@ function getDomElements() {
 		viewChangelogButton: document.getElementById("viewChangelogButton"),
 	}
 }
+
+// Reload all YouTube pages button
+domElements.reloadAllYouTubePagesButton.addEventListener("click", async function () {
+	// Reload all YouTube tabs
+	let tabs = await chrome.tabs.query({});
+	// Split the url and check if the domain is "youtube"
+	for (let i = 0; i <= tabs.length - 1; i++) {
+		if (tabs[i].url.split("/")[2].includes("youtube")) {
+			chrome.tabs.reload(tabs[i].id);
+		}
+	}
+
+	domElements.reloadAllYouTubePagesButton.classList.remove("highlight-green");
+	// Display text after reloading
+	domElements.reloadText.innerHTML = `
+		<br />
+		<br />
+		That's it - 'Shuffle' buttons have been added to all YouTube channel, video and shorts pages!<br />
+		If you experience any issues, feel free to reach out to me on GitHub, linked below and on the popup.
+	`;
+});
 
 // View changelog button
 domElements.viewChangelogButton.addEventListener("click", async function () {
