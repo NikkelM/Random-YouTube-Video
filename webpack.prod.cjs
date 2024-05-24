328a2227b7e2eb4cf180ecdb4683e5e286f65e52
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-05-24 20:17:10 +0100

    Enable distinction between production and development logs

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -4,6 +4,7 @@ const common = require('./webpack.common.cjs');
 module.exports = env => {
 	let mode = "production";
 	env.mode = mode;
+	env.NODE_ENV = mode;
 
 	return merge(common(env), {
 		mode
