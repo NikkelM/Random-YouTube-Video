062d674471444e124d5972cec209963c9a31e8a7
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-07-03 19:30:48 +0100

    Cleaner separations between tests, test cleanup afterEach, allow retries

--- test/compatibility.headful.test.js
+++ test/compatibility.headful.test.js
@@ -9,47 +9,68 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 describe("headful compatibility", function () {
 	this.timeout(30000);
 
-	context("YouTube", function () {
-		context("shuffle button insertion", function () {
-			let browser, page;
+	context("shuffle button insertion", function () {
+		let browser, page;
 
-			beforeEach(async () => {
-				const extensionPath = join(__dirname, "../dist/chromium");
+		before(async () => {
+			const extensionPath = join(__dirname, "../dist/chromium");
 
-				browser = await puppeteer.launch({
-					headless: false, // Extensions only work in head-full mode
-					args: [
-						`--disable-extensions-except=${extensionPath}`,
-						`--load-extension=${extensionPath}`,
-						'--no-sandbox'
-					],
-					executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-				});
-				page = await browser.newPage();
-
-				// Set the SOCS cookie for YouTube (cookie banner)
-				await page.setCookie({
-					'name': 'SOCS',
-					'value': 'CAESEwgDEgk0ODE3Nzk3MjQaAmVuIAEaBgiA_LyaBg', // base64 encoded value
-					'domain': '.youtube.com',
-					'path': '/',
-					'secure': true,
-					'httpOnly': false
-				});
+			browser = await puppeteer.launch({
+				headless: false, // Extensions only work in head-full mode
+				args: [
+					`--disable-extensions-except=${extensionPath}`,
+					`--load-extension=${extensionPath}`,
+					'--no-sandbox'
+				],
+				executablePath: process.env.PUPPETEER_EXEC_PATH,
 			});
+			page = await browser.newPage();
 
-			afterEach(async () => {
-				await browser.close();
+			// Set the SOCS cookie for YouTube (cookie banner)
+			await page.setCookie({
+				'name': 'SOCS',
+				'value': 'CAESEwgDEgk0ODE3Nzk3MjQaAmVuIAEaBgiA_LyaBg', // base64 encoded value
+				'domain': '.youtube.com',
+				'path': '/',
+				'secure': true,
+				'httpOnly': false
 			});
+		});
 
-			it('should insert the shuffle button into the channel header', async function () {
-				await page.goto("https://www.youtube.com/@RickAstleyYT");
+		after(async () => {
+			await browser.close();
+		});
 
-				await page.waitForSelector("#youtube-random-video-large-shuffle-button-channel");
-				const shuffleButton = await page.$("#youtube-random-video-large-shuffle-button-channel");
+		afterEach(async () => {
+			await page.close();
+			page = await browser.newPage();
+		});
 
-				expect(shuffleButton).to.not.be(null);
-			});
+		it('should insert the shuffle button into channel pages', async function () {
+			await page.goto("https://www.youtube.com/@RickAstleyYT");
+
+			await page.waitForSelector("#youtube-random-video-large-shuffle-button-channel");
+			const shuffleButton = await page.$("#youtube-random-video-large-shuffle-button-channel");
+
+			expect(shuffleButton).to.not.be(null);
+		});
+
+		it('should insert the shuffle button into video pages', async function () {
+			await page.goto("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+			await page.waitForSelector("#youtube-random-video-large-shuffle-button-video");
+			const shuffleButton = await page.$("#youtube-random-video-large-shuffle-button-video");
+
+			expect(shuffleButton).to.not.be(null);
+		});
+
+		it('should insert the shuffle button into shorts pages', async function () {
+			await page.goto("https://www.youtube.com/shorts/vOSD7vSreXA");
+
+			await page.waitForSelector("#youtube-random-video-small-shuffle-button-short");
+			const shuffleButton = await page.$("#youtube-random-video-small-shuffle-button-short");
+
+			expect(shuffleButton).to.not.be(null);
 		});
 	});
 });
---
+++ test/compatibility.test.js
@@ -0,0 +1,13 @@
+import expect from "expect.js";
+
+describe("compatibility", function () {
+		context("YouTube URLs", function () {
+			it("should redirect a watch_videos URL to a temporary playlist URL", async function () {
+				const watchVideosUrl = "https://www.youtube.com/watch_videos?video_ids=dQw4w9WgXcQ,dQw4w9WgXcQ,dQw4w9WgXcQ,dQw4w9WgXcQ";
+				const redirectedUrl = (await fetch(watchVideosUrl)).url;
+
+				// The playlist is temporary, indicated by a TL start in the ID
+				expect(redirectedUrl).to.contain("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=TL");
+			});
+		});
+});
